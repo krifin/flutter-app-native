ebaa525375bad70b8ea2a0319779560fd1753ad5
Siddharth Chopra <siddharthchopra@Siddharths-MacBook-Pro.local>
2023-05-30 22:00:10 +0530

    email signup login new ui updated

--- lib/emailLogin.dart
+++ lib/emailLogin.dart
@@ -57,128 +57,146 @@ class _EmailLoginState extends State<EmailLogin> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Container(
-          width: MediaQuery.of(context).size.width * 0.9,
-          height: 350,
-          margin: EdgeInsets.all(20.0),
-          padding: EdgeInsets.all(25.0),
+        backgroundColor: Colors.black,
+        body: Container(
           decoration: BoxDecoration(
-            color: Color(0xff19191B),
-            borderRadius: BorderRadius.circular(35.0),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Row(
-                children: [
-                  IconButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      icon: Icon(
-                        Icons.arrow_back,
-                        color: Colors.grey,
-                      )),
-                  SizedBox(
-                    width: 12,
-                  ),
-                  Text(
-                    'METAMAAP',
-                    style: TextStyle(
-                        fontSize: 24,
-                        color: Colors.white,
-                        fontFamily: 'BeVeitnamPro',
-                        letterSpacing: 10),
-                  ),
-                ],
-              ),
-              Container(
-                  child: Column(
-                children: [
-                  TextField(
-                    style: TextStyle(color: Colors.white),
-                    controller: _emailController,
-                    decoration: InputDecoration(
-                      hintText: 'Your Email',
-                      hintStyle: TextStyle(color: Colors.white70),
-                      filled: true,
-                      fillColor: Color(0xff555556),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(27.0),
-                        borderSide: BorderSide.none,
-                      ),
-                      contentPadding: EdgeInsets.symmetric(
-                          horizontal: 16.0, vertical: 12.0),
-                    ),
-                  ),
-                  SizedBox(height: 15.0),
-                  TextField(
-                    controller: _passwordController,
-                    style: TextStyle(color: Colors.white),
-                    obscureText: _obscureText,
-                    decoration: InputDecoration(
-                      hintText: 'Password',
-                      hintStyle: TextStyle(color: Colors.white70),
-                      filled: true,
-                      fillColor: Color(0xff555556),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(27.0),
-                        borderSide: BorderSide.none,
-                      ),
-                      contentPadding: EdgeInsets.symmetric(
-                          horizontal: 16.0, vertical: 12.0),
-                      suffixIcon: GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            _obscureText = !_obscureText;
-                          });
-                        },
-                        child: Icon(
-                          _obscureText
-                              ? Icons.visibility_off
-                              : Icons.visibility,
-                          color: Colors.grey[600],
-                        ),
-                      ),
-                    ),
-                  ),
+              image: DecorationImage(
+            image: AssetImage("assets/background.png"),
+            fit: BoxFit.cover,
+          )),
+          child: Center(
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.9,
+              height: 350,
+              margin: EdgeInsets.all(20.0),
+              padding: EdgeInsets.all(25.0),
+              // decoration: BoxDecoration(
+              //   color: Color(0xff19191B),
+              //   borderRadius: BorderRadius.circular(35.0),
+              // ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
                   Row(
                     children: [
-                      Expanded(child: Container()),
-                      TextButton(
-                          onPressed: () {},
-                          child: Text(
-                            "Forget Password?",
-                            style: TextStyle(color: Colors.white70),
-                          ))
+                      IconButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          icon: Icon(
+                            Icons.arrow_back,
+                            color: Colors.grey,
+                          )),
+                      SizedBox(
+                        width: 12,
+                      ),
+                      Text(
+                        'METAMAAP',
+                        style: TextStyle(
+                            fontSize: 24,
+                            color: Colors.white,
+                            fontFamily: 'BeVeitnamPro',
+                            letterSpacing: 10),
+                      ),
                     ],
+                  ),
+                  Container(
+                      child: Column(
+                    children: [
+                      TextField(
+                        style: TextStyle(color: Colors.white),
+                        controller: _emailController,
+                        decoration: InputDecoration(
+                          hintText: 'Your Email',
+                          hintStyle: TextStyle(color: Colors.white70),
+                          filled: true,
+                          fillColor: Color(0xff555556).withOpacity(0.5),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.white, width: 0.5),
+                            borderRadius: BorderRadius.circular(27.0),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.white, width: 0.5),
+                            borderRadius: BorderRadius.circular(27.0),
+                          ),
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 16.0, vertical: 12.0),
+                        ),
+                      ),
+                      SizedBox(height: 15.0),
+                      TextField(
+                        controller: _passwordController,
+                        style: TextStyle(color: Colors.white),
+                        obscureText: _obscureText,
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                          hintStyle: TextStyle(color: Colors.white70),
+                          filled: true,
+                          fillColor: Color(0xff555556).withOpacity(0.5),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.white, width: 0.5),
+                            borderRadius: BorderRadius.circular(27.0),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.white, width: 0.5),
+                            borderRadius: BorderRadius.circular(27.0),
+                          ),
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 16.0, vertical: 12.0),
+                          suffixIcon: GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                _obscureText = !_obscureText;
+                              });
+                            },
+                            child: Icon(
+                              _obscureText
+                                  ? Icons.visibility_off
+                                  : Icons.visibility,
+                              color: Colors.grey[600],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Expanded(child: Container()),
+                          TextButton(
+                              onPressed: () {},
+                              child: Text(
+                                "Forget Password?",
+                                style: TextStyle(color: Colors.white70),
+                              ))
+                        ],
+                      )
+                    ],
+                  )),
+                  SizedBox(
+                    width: 100,
+                    height: 40,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xff7B61FF),
+                        onPrimary: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                      ),
+                      onPressed: () {
+                        _login();
+                        print('Button Pressed');
+                      },
+                      child: Text('Login'),
+                    ),
                   )
                 ],
-              )),
-              SizedBox(
-                width: 100,
-                height: 40,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: Color(0xff7B61FF),
-                    onPrimary: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                  ),
-                  onPressed: () {
-                    _login();
-                    print('Button Pressed');
-                  },
-                  child: Text('Login'),
-                ),
-              )
-            ],
+              ),
+            ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
--- lib/emailSignup.dart
+++ lib/emailSignup.dart
@@ -43,164 +43,194 @@ class _EmailSignupState extends State<EmailSignup> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Container(
-          width: MediaQuery.of(context).size.width * 0.9,
-          height: 450,
-          margin: EdgeInsets.all(20.0),
-          padding: EdgeInsets.all(25.0),
+        backgroundColor: Colors.black,
+        body: Container(
           decoration: BoxDecoration(
-            color: Color(0xff19191B),
-            borderRadius: BorderRadius.circular(35.0),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Row(
-                children: [
-                  IconButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      icon: Icon(
-                        Icons.arrow_back,
-                        color: Colors.grey,
-                      )),
+              image: DecorationImage(
+            image: AssetImage("assets/background.png"),
+            fit: BoxFit.cover,
+          )),
+          child: Center(
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.9,
+              height: 450,
+              margin: EdgeInsets.all(20.0),
+              padding: EdgeInsets.all(25.0),
+              // decoration: BoxDecoration(
+              //   color: Color(0xff19191B),
+              //   borderRadius: BorderRadius.circular(35.0),
+              // ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Row(
+                    children: [
+                      IconButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          icon: Icon(
+                            Icons.arrow_back,
+                            color: Colors.grey,
+                          )),
+                      SizedBox(
+                        width: 12,
+                      ),
+                      Text(
+                        'METAMAAP',
+                        style: TextStyle(
+                            fontSize: 24,
+                            color: Colors.white,
+                            fontFamily: 'BeVeitnamPro',
+                            letterSpacing: 10),
+                      ),
+                    ],
+                  ),
+                  Container(
+                      child: Column(
+                    children: [
+                      TextField(
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                          hintText: 'Your Name',
+                          hintStyle: TextStyle(color: Colors.white70),
+                          filled: true,
+                          fillColor: Color(0xff555556).withOpacity(0.5),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.white, width: 0.5),
+                            borderRadius: BorderRadius.circular(27.0),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.white, width: 0.5),
+                            borderRadius: BorderRadius.circular(27.0),
+                          ),
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 16.0, vertical: 12.0),
+                        ),
+                      ),
+                      SizedBox(height: 15.0),
+                      TextField(
+                        controller: _emailController,
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                          hintText: 'Your Email',
+                          hintStyle: TextStyle(color: Colors.white70),
+                          filled: true,
+                          fillColor: Color(0xff555556).withOpacity(0.5),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.white, width: 0.5),
+                            borderRadius: BorderRadius.circular(27.0),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.white, width: 0.5),
+                            borderRadius: BorderRadius.circular(27.0),
+                          ),
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 16.0, vertical: 12.0),
+                        ),
+                      ),
+                      SizedBox(height: 15.0),
+                      TextField(
+                        controller: _passwordController,
+                        style: TextStyle(color: Colors.white),
+                        obscureText: _obscureText,
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                          hintStyle: TextStyle(color: Colors.white70),
+                          filled: true,
+                          fillColor: Color(0xff555556).withOpacity(0.5),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.white, width: 0.5),
+                            borderRadius: BorderRadius.circular(27.0),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.white, width: 0.5),
+                            borderRadius: BorderRadius.circular(27.0),
+                          ),
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 16.0, vertical: 12.0),
+                          suffixIcon: GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                _obscureText = !_obscureText;
+                              });
+                            },
+                            child: Icon(
+                              _obscureText
+                                  ? Icons.visibility_off
+                                  : Icons.visibility,
+                              color: Colors.grey[600],
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 15.0),
+                      TextField(
+                        style: TextStyle(color: Colors.white),
+                        obscureText: _obscureTextComnfirm,
+                        decoration: InputDecoration(
+                          hintText: 'Confirm Password',
+                          hintStyle: TextStyle(color: Colors.white70),
+                          filled: true,
+                          fillColor: Color(0xff555556).withOpacity(0.5),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.white, width: 0.5),
+                            borderRadius: BorderRadius.circular(27.0),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.white, width: 0.5),
+                            borderRadius: BorderRadius.circular(27.0),
+                          ),
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 16.0, vertical: 12.0),
+                          suffixIcon: GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                _obscureTextComnfirm = !_obscureTextComnfirm;
+                              });
+                            },
+                            child: Icon(
+                              _obscureTextComnfirm
+                                  ? Icons.visibility_off
+                                  : Icons.visibility,
+                              color: Colors.grey[600],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )),
                   SizedBox(
-                    width: 12,
-                  ),
-                  Text(
-                    'METAMAAP',
-                    style: TextStyle(
-                        fontSize: 24,
-                        color: Colors.white,
-                        fontFamily: 'BeVeitnamPro',
-                        letterSpacing: 10),
-                  ),
+                    width: 100,
+                    height: 40,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xff7B61FF),
+                        onPrimary: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                      ),
+                      onPressed: () {
+                        print('Button Pressed');
+                        _signup();
+                        Navigator.pushNamed(context, "emailLogin");
+                      },
+                      child: Text('Signup'),
+                    ),
+                  )
                 ],
               ),
-              Container(
-                  child: Column(
-                children: [
-                  TextField(
-                    style: TextStyle(color: Colors.white),
-                    decoration: InputDecoration(
-                      hintText: 'Your Name',
-                      hintStyle: TextStyle(color: Colors.white70),
-                      filled: true,
-                      fillColor: Color(0xff555556),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(27.0),
-                        borderSide: BorderSide.none,
-                      ),
-                      contentPadding: EdgeInsets.symmetric(
-                          horizontal: 16.0, vertical: 12.0),
-                    ),
-                  ),
-                  SizedBox(height: 15.0),
-                  TextField(
-                    controller: _emailController,
-                    style: TextStyle(color: Colors.white),
-                    decoration: InputDecoration(
-                      hintText: 'Your Email',
-                      hintStyle: TextStyle(color: Colors.white70),
-                      filled: true,
-                      fillColor: Color(0xff555556),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(27.0),
-                        borderSide: BorderSide.none,
-                      ),
-                      contentPadding: EdgeInsets.symmetric(
-                          horizontal: 16.0, vertical: 12.0),
-                    ),
-                  ),
-                  SizedBox(height: 15.0),
-                  TextField(
-                    controller: _passwordController,
-                    style: TextStyle(color: Colors.white),
-                    obscureText: _obscureText,
-                    decoration: InputDecoration(
-                      hintText: 'Password',
-                      hintStyle: TextStyle(color: Colors.white70),
-                      filled: true,
-                      fillColor: Color(0xff555556),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(27.0),
-                        borderSide: BorderSide.none,
-                      ),
-                      contentPadding: EdgeInsets.symmetric(
-                          horizontal: 16.0, vertical: 12.0),
-                      suffixIcon: GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            _obscureText = !_obscureText;
-                          });
-                        },
-                        child: Icon(
-                          _obscureText
-                              ? Icons.visibility_off
-                              : Icons.visibility,
-                          color: Colors.grey[600],
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 15.0),
-                  TextField(
-                    style: TextStyle(color: Colors.white),
-                    obscureText: _obscureTextComnfirm,
-                    decoration: InputDecoration(
-                      hintText: 'Confirm Password',
-                      hintStyle: TextStyle(color: Colors.white70),
-                      filled: true,
-                      fillColor: Color(0xff555556),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(27.0),
-                        borderSide: BorderSide.none,
-                      ),
-                      contentPadding: EdgeInsets.symmetric(
-                          horizontal: 16.0, vertical: 12.0),
-                      suffixIcon: GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            _obscureTextComnfirm = !_obscureTextComnfirm;
-                          });
-                        },
-                        child: Icon(
-                          _obscureTextComnfirm
-                              ? Icons.visibility_off
-                              : Icons.visibility,
-                          color: Colors.grey[600],
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              )),
-              SizedBox(
-                width: 100,
-                height: 40,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: Color(0xff7B61FF),
-                    onPrimary: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                  ),
-                  onPressed: () {
-                    print('Button Pressed');
-                    _signup();
-                    Navigator.pushNamed(context, "emailLogin");
-                  },
-                  child: Text('Signup'),
-                ),
-              )
-            ],
+            ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
